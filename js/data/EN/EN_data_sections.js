0ce93c879f6a7ddaba6b19655d11d4fa1075795c
Rémy Fayard <remyfayard3@gmail.com>
2021-07-20 09:39:08 +0200

    fixed english missing _data_sections.js
    Added missing sections in EN_data_sections.js

--- js/data/EN/EN_data_sections.js
+++ js/data/EN/EN_data_sections.js
@@ -1,8 +1,53 @@
 EN_data_movement_title = [
     {
         title: "Movement",
-        max: "Blegh",
-        subtitle: "Testing",
+        max: "Limited by movement speed",
+        subtitle: "You can move at any time during your turn (before, after, or during actions).",
         subtitleId: "basic-movement"
+    }
+]
+
+EN_data_action_title = [
+    {
+        title: "Action",
+        max: "1/turn",
+        subtitle: "You can also interact with one object or feature of the environment for free.",
+        subtitleId: "basic-actions"
+    }
+]
+
+EN_data_bonus_action_title = [
+    {
+        title: "Bonus action",
+        max: "Max. 1/turn",
+        subtitle: "You can take a bonus action only when a special ability, spell, or feature states that you can do something as a bonus action.",
+        subtitleId: "basic-bonus-actions"
+    }
+]
+
+EN_data_reaction_title = [
+    {
+        title: "Reaction",
+        max: "Max. 1/round",
+        subtitle: "A reaction is an instant response to a trigger of some kind, which can occur on your turn or on someone else's.",
+        subtitleId: "basic-reactions"
+    }
+]
+
+EN_data_condition_title = [
+    {
+        title: "Condition",
+        max: "",
+        subtitle: "Conditions alter your capabilities in a variety of ways, and can arise as a result of a spell, a class feature, a monster's attack, or other effect.",
+        subtitleId: "basic-conditions"
+    }
+]
+
+EN_data_environment_title = [
+    {
+        title: "Environmental effects",
+        max: "",
+        subtitle: "Effects that obscure vision can prove a significant hindrance to most adventuring tasks.",
+        subtitleId: "environment-obscurance"
     }
 ]
