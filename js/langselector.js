deaf52ee4cbd31e69d6c3258f9e57bdd3a58d9a8
Rémy Fayard <remyfayard3@gmail.com>
2021-07-21 17:25:50 +0200

    Revert "wip responsive text"
    This reverts commit aa0999e47f61663765a7274ced812012546ce052.

--- js/langselector.js
+++ js/langselector.js
@@ -1,20 +1,22 @@
-let srcBase = ["data_action.js", "data_bonusaction.js", "data_condition.js", "data_environment.js", "data_movement.js", "data_reaction.js", "data_sections.js"];
+// import { load } from "quickref.js";
+
+let srcBase = ["data_action.js", "data_bonusaction.js", "data_condition.js", "data_environment.js", "data_movement.js", "data_reaction.js", "data_sections.js"]
 
 function selLang(sel) {
-    let sourceBase = "js/data/" + sel.value + "/";
+    let sourceBase = "js/data/" + sel.value + "/"
     console.log("sel =", sel);
-    console.log("selected =", sel.value);
+    console.log("selected =", sel.value)
     switch (sel.value) {
         case "EN":
-            console.log("Case EN");
+            console.log("Case EN")
             break;
         case "FR":
-            console.log("Case FR");
+            console.log("Case FR")
             if ($("#" + sel.value + "_firstDataScript").length) {
                 console.log(sel.value + " already loaded");
             } else {
                 srcBase.forEach((data, index) => {
-                    console.log("passage case ", sel.value, "|", "data =", data, "selected =", sel.value);
+                    console.log("passage case ", sel.value, "|", "data =", data, "selected =", sel.value)
                     let s = document.createElement("script");
                     if (index === 0) {
                         s.id = sel.value + "_firstDataScript";
--- js/quickref.js
+++ js/quickref.js
@@ -7,7 +7,7 @@ function add_quickref_item(parent, data, type) {
     var item = document.createElement("div");
     item.className += "item itemsize"
     item.innerHTML =
-        '\
+    '\
     <div class="item-icon iconsize icon-' + icon + '"></div>\
     <div class="item-text-container text">\
         <div class="item-title">' + title + '</div>\
@@ -19,7 +19,7 @@ function add_quickref_item(parent, data, type) {
     var color = style.backgroundColor;
 
     item.onclick = function () {
-        show_modal(data, color, bType);
+        show_modal(data, color,bType);
     }
 
     parent.appendChild(item);
@@ -41,9 +41,7 @@ function show_modal(data, color, type) {
     $("#modal-subtitle").text(subtitle);
     $("#modal-reference").text(reference);
 
-    var bullets_html = bullets.map(function (item) {
-        return "<p class=\"fonstsize\">" + item + "</p>";
-    }).join("\n<hr>\n");
+    var bullets_html = bullets.map(function (item) { return "<p class=\"fonstsize\">" + item + "</p>"; }).join("\n<hr>\n");
     $("#modal-bullets").html(bullets_html);
 }
 
@@ -56,7 +54,7 @@ function fill_section(data, parentname, type) {
     empty_section(parentname);
     var parent = document.getElementById(parentname);
 
-
+    
     // data.sort(dynamicSort("title"));
 
     // console.log("console.log de data", data);
@@ -68,31 +66,31 @@ function fill_section(data, parentname, type) {
 
 function empty_section(parentname) {
     var parent = document.getElementById(parentname);
-    parent.innerHTML = "";
+        parent.innerHTML = "";
 }
 
 function add_base_data(parent, data) {
-    var items = [];
+        var items = [];
 
-    Object.entries(data).forEach(entry => {
-        const [key, value] = entry;
-        let item = document.createElement("div");
-        switch (key) {
-            case "title":
-                item.className = "section-title";
-                item.innerHTML = value + ' ' + "<span class='float-right'>" + data.max + "</span>";
-                items.push(item);
-                break;
-            case "subtitle":
-                item.className = "section-content";
-                item.innerHTML = "<div class='section-row section-subtitle text fontsize'>" + value + "</div> <div class='section-row' id=" + data.subtitleId + "></div>";
-                items.push(item);
-                break;
-        }
-    });
-    items.forEach(child => {
-        parent.appendChild(child);
-    })
+        Object.entries(data).forEach(entry => {
+            const [key, value] = entry;
+            let item = document.createElement("div");
+            switch(key) {
+                case "title":
+                    item.className = "section-title";
+                    item.innerHTML = value + ' ' + "<span class='float-right'>" + data.max + "</span>";
+                    items.push(item);
+                    break;
+                case "subtitle":
+                    item.className = "section-content";
+                    item.innerHTML = "<div class='section-row section-subtitle text fontsize'>" + value + "</div> <div class='section-row' id=" + data.subtitleId + "></div>";
+                    items.push(item);
+                    break;
+            }
+          });
+        items.forEach(child => {
+            parent.appendChild(child);
+        })
 }
 
 function fill_section_base(data, parentname) {
@@ -108,13 +106,13 @@ function fill_section_base(data, parentname) {
 function dynamicSort(property) {
     let sortOrder = 1;
 
-    if (property[0] === "-") {
+    if(property[0] === "-") {
         sortOrder = -1;
         property = property.substr(1);
     }
 
-    return function (a, b) {
-        if (sortOrder == -1) {
+    return function(a,b) {
+        if(sortOrder == -1) {
             return b[property].localeCompare(a[property]);
         } else {
             return a[property].localeCompare(b[property]);
@@ -139,7 +137,7 @@ function init(lang) {
     fill_section_base(window[lang + "_data_condition_title"], "section-condition");
     fill_section_base(window[lang + "_data_environment_title"], "section-environment");
 
-    console.log("what is data= ", window[lang + "_data_movement"], "héhé = ", lang + "_data_movement")
+    console.log("what is data= ", window[lang + "_data_movement"], "héhé = ", lang + "_data_movement" )
     fill_section(window[lang + "_data_movement"], "basic-movement", "Move");
     fill_section(window[lang + "_data_action"], "basic-actions", "Action");
     fill_section(window[lang + "_data_bonusaction"], "basic-bonus-actions", "Bonus action");
@@ -154,8 +152,8 @@ function init(lang) {
     modal.onclick = hide_modal;
 }
 
-$('body').flowtype();
-
-$("select").on("change", function () {
+$("select").on("change", function() {
     selLang(this);
 }).change();
+
+// $(window).load(init);
